8252583c6a64d564a43db6a178cbaf16d35b7a9f
Ezequiel Tejerina <tejerinae@gmail.com>
2023-01-12 15:24:56 -0300

    comment

--- crud/views.py
+++ crud/views.py
@@ -3,6 +3,17 @@ from .models import AddressBook
 from django.contrib import messages
 
 def validateEmail(email):
+    """validate emails.
+    Could have used email_validator library or regex, but decided to do it this way 
+    to show some code, string and list handling
+
+    Args:
+        email (_type_): str
+
+    Returns:
+        _type_: [str]
+    """
+    
     errors = []
     if not '@' in email:
         errors.append('The email must contain "@"!')
