19976a4514d4973932ac5fc60e882bf7279b66fc
Ezequiel Tejerina <tejerinae@gmail.com>
2023-01-12 16:31:00 -0300

    test

--- crud/tests.py
+++ crud/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .utils import validateEmail
 
 # Create your tests here.
+class TestEmails(TestCase):
+    def test_email_validation_without_at(self):
+        errors = validateEmail('test#user.com')
+        self.assertIsInstance(errors, list)
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0], 'The email must contain "@"!')
+
+    def test_email_validation_with_wrong_domain(self):
+        errors = validateEmail('test@com')
+        self.assertIsInstance(errors, list)
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0], 'The email has no valid format!')
